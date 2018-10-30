b645610731c59ba9a17c34600c2265dd4d9625e9
s2motion.developer <s2motion.developer@gmail.com>
2018-10-31 00:19:15 +0900

    add filter_sequence.py

---
+++ src/test/filter_sequence.py
@@ -0,0 +1,17 @@
+mylist = [1, 4, -5, 10, -7, 2, 3, -1]
+ps = (n for n in mylist if n > 0)
+
+for x in ps:
+	print(x)
+
+values = ['1', '2', '-3', '-', '4', 'N/A', '5']
+
+def is_int(val):
+	try:
+		x = int(val)
+		return True
+	except ValueError:
+		return False
+
+ivals = list(filter(is_int, values))
+print(ivals)
